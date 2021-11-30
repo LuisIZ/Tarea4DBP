7723075e0ffba04966c06c7a7ba152a83ccdeaf4
LuisIZ <luis.izaguirre@utec.edu.pe>
2021-11-30 16:24:26 -0500

    Se agrega Tarea 4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,8 +13,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,ReactiveFormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
